74c94b516127ea761d19dfc6d591714fa72cd313
Dunstan M'mbehero <dunmmbehero@gmail.com>
2021-06-08 12:26:16 +0300

    create reception model

--- Huduma-Afya/HudumaApp/models.py
+++ Huduma-Afya/HudumaApp/models.py
@@ -41,3 +41,16 @@ class Appointment(models.Model):
 	
 	def __str__(self):
 		return self.patientname+" you have appointment with "+self.doctorname
+
+class Receptionist(models.Model):
+	name = models.CharField(max_length=50)
+	email = models.EmailField(unique=True)
+	password = models.CharField(max_length=16)
+	gender = models.CharField(max_length=10)
+	phonenumber = models.CharField(max_length=10)
+	address = models.CharField(max_length=100)
+	birthdate = models.DateField()
+	bloodgroup = models.CharField(max_length=5)
+
+	def __str__(self):
+		return self.name
