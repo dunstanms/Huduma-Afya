e709b9ca0ac459f0ec89461fae559af57445e39e
Dunstan M'mbehero <dunmmbehero@gmail.com>
2021-06-08 12:23:42 +0300

    create Patient model

--- Huduma-Afya/HudumaApp/models.py
+++ Huduma-Afya/HudumaApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Doctor(models.Model):
+	name = models.CharField(max_length=50)
+	email = models.EmailField(unique=True)
+	password = models.CharField(max_length=16)
+	gender = models.CharField(max_length=10)
+	phonenumber = models.CharField(max_length=10)
+	address = models.CharField(max_length=100)
+	birthdate = models.DateField()
+	bloodgroup = models.CharField(max_length=5)
+	specialization = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name
+
+class Patient(models.Model):
+	name = models.CharField(max_length=50)
+	email = models.EmailField(unique=True)
+	password = models.CharField(max_length=16)
+	gender = models.CharField(max_length=10)
+	phonenumber = models.CharField(max_length=10)
+	address = models.CharField(max_length=100)
+	birthdate = models.DateField()
+	bloodgroup = models.CharField(max_length=5)
+
+	def __str__(self):
+		return self.name
+
