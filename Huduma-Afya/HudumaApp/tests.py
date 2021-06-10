de986e463c64f9651a25df8a7b8f3a2df7f87cce
Dunstan M'mbehero <dunmmbehero@gmail.com>
2021-06-10 11:46:16 +0300

    create tests

--- Huduma-Afya/HudumaApp/tests.py
+++ Huduma-Afya/HudumaApp/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import *
 
-# Create your tests here.
+
+
+class ProfileTestClass(TestCase):
+    #Set up method
+
+    def setUp(self):
+        
+        self.new_profile = Profile(user_id=2,bio=" testing", email='dan@mail.com',name="admin",profile_pic="default.jpeg")
+
+  
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+    
+    def test_save_method(self):
+        self.new_profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>0)
+
+    def test_delete_method(self):
+        self.new_profile.save_profile()
+        self.new_profile.delete_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)==0) 
+    
+    def tearDown(self):
+        Profile.objects.all().delete()
